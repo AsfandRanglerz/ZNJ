c149c6f484261869b63504c7eadcd68bf4c8cdb2
mhsamiullah007 <117739853+mhsamiullah007@users.noreply.github.com>
2022-11-14 10:45:33 +0500

    changes

--- resources/views/emails/reset_password_user.blade.php
+++ resources/views/emails/reset_password_user.blade.php
@@ -1,13 +1,13 @@
 @component('mail::message')
 <img src="{{ asset('public/admin/assets/img/logo.png')}}" alt="ZNJ Logo" style="max-width: 17%; left:50%; margin-bottom: 23px;">
 
-<h1>Forget Password</h1>
+<h1 style=" left:30%; margin-bottom: 23px;">Forget Password</h1>
 
 
 
 You are receiving this email because we received a password reset request for your account.
 
-<strong style="max-width: 10%; left:50%; margin-bottom: 10px;font-size:25px;">{{ $otp }}</strong>
+<strong style="left:46%; margin-bottom: 10px;font-size:25px;">{{ $otp }}</strong>
 
 This password reset OTP will expire in 60 minutes.
 
