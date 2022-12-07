00f117653969d7e4f4724734c7c03441c63ae715
UsmanRanglerz <usman.ranglerz449@gmail.com>
2022-12-07 11:03:31 +0500

    changes on admin pane

---
+++ app/Http/Controllers/Api/SearchController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use App\Models\Event;
+use App\Models\Venue;
+use Illuminate\Http\Request;
+use App\Models\EntertainerDetail;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class SearchController extends Controller
+{
+    public function searchData(Request $request)
+    {
+        $data['events'] = Event::where('title', 'like', '%' . $request->search . '%')->get();
+        $data['entertainers'] = EntertainerDetail::where('title', 'like', '%' . $request->search . '%')->get();
+        $data['venues'] = Venue::where('title', 'like', '%' . $request->search . '%')->get();
+        return $this->sendSuccess('Success', compact('data'));
+    }
+
+    public function searchFilter(Request $request)
+    {
+        if ($request->type == 'entertainer') {
+            $data['entertainers'] = EntertainerDetail::where('title', 'like', '%' . $request->search . '%')->get();
+            return $this->sendSuccess('Success', compact('data'));
+        } elseif ($request->type == 'event') {
+            $data['events'] = Event::where('title', 'like', '%' . $request->search . '%')->get();
+            return $this->sendSuccess('Success', compact('data'));
+        } elseif ($request->type == 'venue') {
+            $data['venues'] = Venue::where('title', 'like', '%' . $request->search . '%')->get();
+            return $this->sendSuccess('Success', compact('data'));
+        } else {
+            $data['events'] = Event::where('title', 'like', '%' . $request->search . '%')->get();
+            $data['entertainers'] = EntertainerDetail::where('title', 'like', '%' . $request->search . '%')->get();
+            $data['venues'] = Venue::where('title', 'like', '%' . $request->search . '%')->get();
+            return $this->sendSuccess('Success', compact('data'));
+        }
+    }
+    public function myAdsFilter(Request $request)
+    {
+        $user=User::find(Auth::id());
+        if ($user->role == 'entertainer') {
+            $data = EntertainerDetail::where('user_id',Auth::id())->where('title', 'like', '%' . $request->search . '%')->get();
+            return $this->sendSuccess('Success', compact('data'));
+        }elseif($user->role == 'event'){
+            $data = Event::where('user_id',Auth::id())->where('title', 'like', '%' . $request->search . '%')->get();
+            return $this->sendSuccess('Success', compact('data'));
+        }elseif($user->role == 'venue'){
+            $data = Venue::where('user_id',Auth::id())->where('title', 'like', '%' . $request->search . '%')->get();
+            return $this->sendSuccess('Success', compact('data'));
+        }else{
+            return $this->sendError('Not Found!');
+        }
+    }
+}
